cba2cd346c5f96d8a47799c94802b90ac44a4e3a
Blake Skaja <bskaja@iastate.edu>
2015-12-09 18:31:22 -0600

    Initial Server

---
+++ simpleserver.php
@@ -0,0 +1,45 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$apiKey = '941d558c-76a3-4e0a-b90a-1c59c34623cd';
+
+if (isset($_REQUEST['action'])) {
+	session_start();
+    switch ($_REQUEST['action']) {
+        case 'CreatePost':
+            getGameData();
+            break;
+		case 'Update':
+            updatePost();
+            break;
+    }
+}
+
+function getGameData() {
+	$summonerName = 'skyman12';
+ 
+	// get the basic summoner info
+	$result = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' . $summonerName . '?api_key=' . $GLOBALS['apiKey'];
+	$summoner = curl_get_contents($result);
+	// $summoner = json_decode(curl_get_contents($result))->$summonerName;
+	echo $summoner;
+}
+
+function getSummonerData() {
+	
+}
+
+function curl_get_contents($url)
+{
+  $ch = curl_init($url);
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
+?>
